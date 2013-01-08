3e737aab018ad8280ee17ee945c89ff672cebf1b
Joe Nelson <cred+github@begriffs.com>
2013-01-07 22:16:16 -0600

    Basic layout and search form

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 gem 'sinatra'
 gem 'rake'
 gem 'pg'
+gem 'haml'
 
 group :development, :test do
   gem 'minitest'
--- app.rb
+++ app.rb
@@ -3,5 +3,14 @@ require 'pg'
 require './init'
 
 get '/' do
-  sql('select version from schema_info').first['version']
+  haml :index
+end
+
+get '/search' do
+  @q = params[:q]
+  @results = sql(
+    "SELECT * FROM expressions WHERE to_tsvector(input || ' ' || value || ' ' || output) @@ to_tsquery($1)",
+    @q
+  )
+  haml :search
 end
